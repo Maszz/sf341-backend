c18711e42d56e93334182adb47e7cc5b13b221ff
Masz <mawin38408@gmail.com>
2022-09-04 23:20:48 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ForbiddenException } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule);
 
   await app.listen(process.env.PORT || 8080);
   console.log(`Application is running on: ${await app.getUrl()}`);
   console.log(
     `Application is listening on: ${await app.getHttpServer().address().port}`,
   );
+  const whitelist = ['http://localhost:3000'];
+
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1 || origin == undefined) {
+        console.log(`${origin}` + ' is allowed');
+        callback(null, true);
+      } else {
+        console.log(`${origin} is not allowed`);
+        callback(new ForbiddenException('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  });
 }
 bootstrap();
