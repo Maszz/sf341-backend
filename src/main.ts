03f16d3e57b935e7efe8b9e2d30642cccb4bc901
Masz <mawin38408@gmail.com>
2022-09-04 22:48:58 +0700

    add console

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(
+    `Application is listening on: ${await app.getHttpServer().address().port}`,
+  );
 }
 bootstrap();
